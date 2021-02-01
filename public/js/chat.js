b34c2d4e6a9182bf526567b6680094107006885b
Jia Qi <04jiaqi@gmail.com>
2021-02-02 09:13:39 +1300

    Render dynamic templates to the browser

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,38 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $location = document.querySelector('#location')
+const $messages = document.querySelector('#messages')
+
+//templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (msg) => {
     console.log(msg)
+    const html = Mustache.render(messageTemplate, {
+        message: msg
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e)=> {
+$messageForm.addEventListener('submit', (e)=> {
     // prevent refresh
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if(error) {
             return console.log(error)
         }
@@ -18,16 +40,21 @@ document.querySelector('#message-form').addEventListener('submit', (e)=> {
     })
 })
 
-document.querySelector('#location').addEventListener('click', () => {
+$location.addEventListener('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser')
     }
+
+    $location.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation',
         {
             longitude: position.coords.longitude, 
             latitude: position.coords.latitude
-        }, () => { console.log("Location delivered")})
+        }, () => { 
+            $location.removeAttribute('disabled')
+            console.log("Location delivered")
+        })
     })
 })
